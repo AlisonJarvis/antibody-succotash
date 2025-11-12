f253f12f1cb8bd0f96a06cb65681456ce5a4dcf9
Colman Bouton <cole.bouton@gmail.com>
2025-11-12 11:08:55 -0800

    Update embedding_utils to return a pd.Series object

--- data_utils/embedding_utils.py
+++ data_utils/embedding_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm.auto import tqdm
 
 
-def get_model_embeddings(sequence_df: pd.DataFrame) -> list[np.ndarray]:
+def get_model_embeddings(sequence_df: pd.DataFrame) -> pd.Series:
     '''
     Given an input `DataFrame` with `"vh_protein_sequence"` and `"vl_protein_sequence"` columns,
     generates an embedding for each token using the p-IgGen transformer model. Returns results as
@@ -15,7 +15,7 @@ def get_model_embeddings(sequence_df: pd.DataFrame) -> list[np.ndarray]:
         sequence_df (DataFrame): The input dataframe containing the vh and vl protein sequences.
 
     Returns:
-        list[np.ndarray]: A list containing embeddings for each token
+        Series: A series with elements consisting of token level embeddings of each sequence.
     '''
     model_name = "ollieturnbull/p-IgGen"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -40,6 +40,6 @@ def get_model_embeddings(sequence_df: pd.DataFrame) -> list[np.ndarray]:
         batch = tokenizer(sequences[i:i+batch_size], return_tensors="pt", padding=True, truncation=True)
         outputs = model(batch["input_ids"].to(device), return_rep_layers=[-1], output_hidden_states=True)
         embeddings = outputs["hidden_states"][-1].detach().cpu().numpy()
-        full_embeddings.append(embeddings)
-    del model
-    return full_embeddings
+        for i in range(embeddings.shape[0]):
+            full_embeddings.append(embeddings[i])
+    return pd.Series(full_embeddings, index=sequence_df.index)
